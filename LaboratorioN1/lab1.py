8d2ec68a85b696877cf7a58b7dd4bcf74ec1a970
MontielNico <montielnicolas848@gmail.com>
2026-04-10 21:30:16 -0300

    gráficos de torta y de lineas

--- LaboratorioN1/lab1.py
+++ LaboratorioN1/lab1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 #Lectura de datos
@@ -8,18 +9,47 @@ df2 = pd.read_csv('datos_ventas_suc2.csv', encoding='latin-1')
 #print(df2)
 
 #Fusión de datos
-print("Df fusionado")
+#print("Df fusionado")
 df_fusionado = pd.concat([df,df2], ignore_index=True)
-print(df_fusionado)
+#print(df_fusionado)
 
 #Tratamiento de datos
 df_fusionado['Fecha'] = pd.to_datetime(df_fusionado['Fecha'])
-print(df_fusionado)
+#print(df_fusionado)
 
 #Análisis de ventas
-print("Tabla cantidad de productos totales")
+#print("Tabla cantidad de productos totales")
 productoMasVendido = df_fusionado.groupby('Producto')['Cantidad'].sum()
-print(productoMasVendido)
-mesVentas = df_fusionado.groupby(df_fusionado['Fecha'].dt.month_name())['Total Venta'].sum()
+#print(productoMasVendido)
 
-print(mesVentas.sort_values(ascending=False))
+ordenMeses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+mesVentas = df_fusionado.groupby(df_fusionado['Fecha'].dt.month_name())['Total Venta'].sum().reset_index()
+mesVentas['Fecha'] = pd.Categorical(mesVentas['Fecha'], categories=ordenMeses, ordered=True)
+mesVentas = mesVentas.sort_values('Fecha')
+print(mesVentas)
+
+
+##Gráfico de torta
+
+ventasPorCategorias = df_fusionado.groupby('Producto')['Total Venta'].sum().reset_index()
+
+#print(ventasPorCategorias)
+
+
+plt.figure(figsize=(8,8))
+
+# Si SI usaste reset_index():
+plt.pie(ventasPorCategorias['Total Venta'], labels=ventasPorCategorias['Producto'], autopct='%1.1f%%', shadow=True)
+plt.title('Ventas por categoría')
+plt.axis('equal')
+
+
+#Gráfico de lineas
+plt.figure(figsize=(10,6))
+plt.plot(mesVentas['Fecha'], mesVentas['Total Venta'], marker='o')
+plt.title('Ventas por mes')
+plt.xlabel('Mes')
+plt.ylabel('Total Venta')
+plt.xticks(rotation=45)
+plt.grid()
+plt.show()  
